56c6d0cb9bcb2893eb31121c46401525442e12ae
Andrés Moran <144814563+AndresGMoran@users.noreply.github.com>
2024-02-29 18:24:15 +0100

    Calculadora

--- src/main/java/com/andresgmoran/Tema_08/Calculadora.java
+++ src/main/java/com/andresgmoran/Tema_08/Calculadora.java
@@ -5,26 +5,39 @@ import java.awt.*;
 
 public class Calculadora {
     public static void main(String[] args) {
-        JFrame ventana = new JFrame("Calculadora");
-
-        Toolkit toolkit = Toolkit.getDefaultToolkit();
-        Dimension dimension = toolkit.getScreenSize();
+        JFrame ventana = new JFrame("Calculator");
 
         int width = 500;
         int height = 500;
-        int x = (int) (dimension.getWidth() / 2f) - Math.round(width / 2f);
-        int y = (int) (dimension.getHeight() / 2f) - Math.round(height / 2f);
+        int x,y;
+
+        Toolkit herramienta = Toolkit.getDefaultToolkit();
+        Dimension dimension =Toolkit.getDefaultToolkit().getScreenSize();
+
+        x = (int)(dimension.getWidth()/2f) - Math.round(width/2f);
+        y = (int)(dimension.getHeight()/2f) - Math.round(height/2f);
+
+        String [] simbols = new String[]
+                {"CE" , "C" , "%" , "x",
+                        "(" , ")" , "MOD" , "/",
+                        "7" , "8" , "9" , "*",
+                        "4" , "5" , "6" , "-",
+                        "1" , "2" , "3" , "+",
+                        "+/-" , "0" , "." , "="};
 
         JPanel panel = new JPanel();
-        GridLayout gridLayout = new GridLayout(5,4);
+        GridLayout gridLayout = new GridLayout(6,4);
         panel.setLayout(gridLayout);
-
-        for (int i = 1; i <= 5*4;i++){
-            JButton button = new JButton("" + i);
+        for (String simbol : simbols) {
+            JButton button = new JButton(simbol);
+            button.setBackground(Color.black);
+            button.setForeground(Color.white);
             panel.add(button);
         }
+
         ventana.setContentPane(panel);
         ventana.setBounds(x,y,width,height);
         ventana.setVisible(true);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
