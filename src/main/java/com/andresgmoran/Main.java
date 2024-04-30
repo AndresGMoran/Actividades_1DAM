92cdaeada6003b811ac996e4a980beebd80e5484
AndresGMoran <moranandres79@gmail.com>
2024-04-30 14:36:19 +0200

    Actividad 10 Tema 10

--- src/main/java/com/andresgmoran/Main.java
+++ src/main/java/com/andresgmoran/Main.java
@@ -5,9 +5,10 @@ import com.andresgmoran.Lib.IOLib;
 import com.andresgmoran.Tema_10.Actividad08.Actividad08;
 import com.andresgmoran.Tema_10.Actividad08.Diccionario;
 import com.andresgmoran.Tema_10.Actividad09.Actividad09;
+import com.andresgmoran.Tema_10.Actividad10.Actividad10;
 
 public class Main {
     public static void main(String[] args) {
-        Actividad09 actividad09 = new Actividad09();
+        Actividad10 actividad10 = new Actividad10();
     }
 }
